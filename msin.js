2418585bca778ba01d2c423f47b574d915c15ad5
artem6742 <144213709+artem6742@users.noreply.github.com>
2024-01-25 11:22:02 +0300

    Add files via upload

---
+++ msin.js
@@ -0,0 +1,160 @@
+// alert, prompt, confirm()
+
+// простая страница
+// let a = prompt('как тебя зовут?');
+// alert(a)
+
+
+
+//постфикстая структура
+// c = 1
+// b = 2
+
+
+//результат присваивания
+// a = 4
+// x = 5
+
+
+// Испрвте сложение
+//         нужно написать alert(Number(a + b));
+
+
+// работа с прермнными
+
+// let num = 3
+// alert(num)
+
+// let a = 10, b = 2
+// alert(a / b, a + b, a - b, a * b);
+
+
+// let a = 15, b = 2
+// alert(result)
+// var result = a + b
+
+
+// let a = 10, b = 2, c = 5
+// alert(Number(a + b + c))
+
+
+// let a = 17, b = 10;
+// let c = a = b ;
+// let d = 7;
+// let result = d + c;
+// alert(result)
+
+
+// работа со строками
+
+
+// let str = 'Привет мир'
+// alert(str)
+
+// let str1 = 'привет', str2 = 'мир'
+// alert(str1 + str2)
+
+// let name = 'artem'
+// alert('hi,'+ name)
+
+//let age = 16
+//alert('мне ' + age + 'лет')
+
+
+// функция prompt
+
+// let a = prompt('my name ')
+// alert('ваше имя' + a)
+
+// let a = prompt('введите чсило')
+// alert(a ** 2)
+
+
+// обрашение к символам строки
+//
+// let str = 'abcde'
+// alert(str[0] + str[2] + str[4])
+
+
+// let number = '12345';
+// let digit1 = parseInt(number[0]);
+// let digit2 = parseInt(number[1]);
+// let digit3 = parseInt(number[2]);
+// let digit4 = parseInt(number[3]);
+// let digit5 = parseInt(number[4]);
+//
+// let product = digit1 * digit2 * digit3 * digit4 * digit5;
+//
+// console.log("Произведение цифр числа 12345: " + product);
+
+
+// практика
+
+// let secondsInHour = 60 * 60;
+//
+// let secondsInDay = secondsInHour * 24;
+//
+// let secondsInMonth = secondsInDay * 30;
+//
+// console.log("Количество секунд в часе: " + secondsInHour);
+// console.log("Количество секунд в сутках: " + secondsInDay);
+// console.log("Приблизительное количество секунд в месяце: " + secondsInMonth);
+
+
+// let hour = 12, minutes = 30, seconds = 30;
+// alert(hour +':'+ minutes + ':'+ seconds)
+//
+//
+// let a = 10
+// alert( a ** 2)
+
+
+// Работа с присваиванием и декриментами
+
+// var num = 47;
+// num += 7;
+// num -= 7;
+// num *= 7;
+// num /= 7;
+// alert(num)
+
+// var num = 10
+// num++
+// num++
+// num--
+// alert(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
